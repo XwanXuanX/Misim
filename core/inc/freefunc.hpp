237fc7a551ba9d673c628c5b5d805b9bcea00df3
XwanXuanX <lzlu1018@gmail.com>
2024-02-10 12:06:04 -0500

    Refactor freefunc.hpp file.

--- core/inc/freefunc.hpp
+++ core/inc/freefunc.hpp
@@ -4,216 +4,254 @@
 #include <concepts>
 #include <cstdint>
 #include <climits>
-#include <variant>
+#include <expected>
 #include <stdexcept>
 
 
-namespace pipeline::freefunc
+namespace misim
 {
-  static_assert(CHAR_BIT == 8u);
 
-  template <std::unsigned_integral U>
-  [[nodiscard]] constexpr bool checkBitInRange(const std::integral auto pos) noexcept
-  {
-    return pos >= 0 && pos < (sizeof(U) * CHAR_BIT);
+namespace freefunc
+{
+
+/**
+ * Code begin.
+*/
+
+#define NODISCARD [[nodiscard]]
+
+// Good to have some sanity check :)
+static_assert(CHAR_BIT == 8u);
+
+// using declarations
+using std::unsigned_integral;
+using std::integral;
+
+// Check if a type is a specialization of some template type
+template <class T, template <class...> class Template>
+struct is_specialization : std::false_type {};
+
+template <template <class...> class Template, class... Args>
+struct is_specialization<Template<Args...>, Template> : std::true_type {};
+
+
+/**
+ * The solution to solve problem with multiply invokes undefined behaviour
+ * 
+ * Explanation:
+ *  Define A & B be unsigned integers, whose size less than signed int (normally 32bits)
+ *  If the product of A * B overflows, A & B will be promoted to signed int and multiply.
+ *  However, if the result overflows, signed int will invoke undefined behaviour.
+ * 
+ * Solution:
+ *  Use template method to let unsigned integers only promote to unsigned int.
+ *  After the operation is done, truncated the result to original size.
+ * 
+*/
+
+template <std::unsigned_integral U>
+struct MakePromoted
+{
+  static constexpr bool small{ sizeof(U) < sizeof(std::uint32_t) };
+
+  using type = std::conditional_t<small, std::uint32_t, U>;
+};
+
+template <typename T>
+using make_promoted_t = typename MakePromoted<T>::type;
+
+template <typename T>
+NODISCARD constexpr
+make_promoted_t<T> promote(const T n) { return n; }
+
+
+/**
+ * Bit checking functions (observers)
+*/
+
+// Check if a bit is in range
+template <unsigned_integral U>
+NODISCARD constexpr
+bool checkBitInRange(const integral auto pos)
+{
+  return pos >= 0 && pos < (sizeof(U) * CHAR_BIT);
+}
+
+// Check if a bit is in range
+NODISCARD constexpr
+bool checkBitInRange(const unsigned_integral auto n, const integral auto pos)
+{
+  return checkBitInRange<decltype(n)>(pos);
+}
+
+
+using std::expected;
+
+template <typename T, typename E = std::domain_error>
+using Expected = expected<T, E>;
+
+// Test if a bit in an unsigned integral is set or no
+template <unsigned_integral U>
+NODISCARD constexpr
+Expected<bool> testBit(const U n, const integral auto pos)
+{
+  if (!freefunc::checkBitInRange(n, pos)) {
+    return std::unexpected(std::domain_error("pos out of range"));
   }
 
-  [[nodiscard]] constexpr bool checkBitInRange(const std::unsigned_integral auto n,
-    const std::integral auto pos) noexcept
-  {
-    return freefunc::checkBitInRange<decltype(n)>(pos);
-  }
+  return static_cast<bool>(n & (static_cast<U>(1) << pos));
+}
 
-  template <typename T, typename Error = std::domain_error>
-  using return_type = std::variant<T, Error>;
+// Test if an unsigned integral is 0b111..111
+NODISCARD constexpr
+Expected<bool> testBitAll(const unsigned_integral auto n)
+{
+  constexpr std::size_t n_size = sizeof(n) * CHAR_BIT - 1;
 
-  using return_bool_t = return_type<bool>;
-
-  template <std::unsigned_integral U>
-  [[nodiscard]] constexpr return_bool_t testBit(const U n, const std::integral auto pos) noexcept
-  {
-    if (!freefunc::checkBitInRange(n, pos)) {
-      return std::domain_error("pos out of range");
-    }
-
-    return static_cast<bool>(n & (static_cast<U>(1) << pos));
-  }
-
-  [[nodiscard]] constexpr return_bool_t testBitAll(const std::unsigned_integral auto n) noexcept
-  {
-    const std::size_t n_size{ sizeof(n) * CHAR_BIT - 1 };
-
-    if constexpr (sizeof(n) == sizeof(std::uintmax_t)) {
-      if (const auto result{ freefunc::testBit(n, n_size) }; result.index() == 0 && !std::get<0>(result)) {
-        return false;
-      } else if (result.index() != 0) {
-        return result;
-      }
-    }
-
-    const std::unsigned_integral auto mask{ (static_cast<std::uintmax_t>(1) << n_size) - 1 };
-    return (n & mask) == mask;
-  }
-
-  [[nodiscard]] constexpr return_bool_t testBitAll(const std::unsigned_integral auto n,
-    const std::integral auto last_bits) noexcept
-  {
-    if (!freefunc::checkBitInRange(n, last_bits)) {
-      return std::domain_error("last_bits out of range.");
-    }
-
-    if (!freefunc::checkBitInRange(n, last_bits + 1)) {
-      return freefunc::testBitAll(n);
-    }
-
-    const std::unsigned_integral auto mask{ (static_cast<std::uintmax_t>(1) << (last_bits + 1)) - 1 };
-    return (n & mask) == mask;
-  }
-
-  [[nodiscard]] constexpr return_bool_t testBitAny(const std::unsigned_integral auto n) noexcept
-  {
-    return static_cast<bool>(n);
-  }
-
-  [[nodiscard]] constexpr return_bool_t testBitAny(const std::unsigned_integral auto n,
-    const std::integral auto last_bits) noexcept
-  {
-    if (!freefunc::checkBitInRange(n, last_bits)) {
-        return std::domain_error("last_bits out of range");
-    }
-
-    if (!freefunc::checkBitInRange(n, last_bits + 1)) {
-        return freefunc::testBitAny(n);
-    }
-    
-    const std::unsigned_integral auto mask{ (static_cast<std::uintmax_t>(1) << (last_bits + 1)) - 1 };
-    return static_cast<bool>(n & mask);
-  }
-
-  template <typename T>
-  concept valid_variant_type = requires(T param)
-  {
-    { param.index() } -> std::convertible_to<std::size_t>;
-    requires std::convertible_to<decltype(std::get<0>(param)), bool>;
-  };
-
-  [[nodiscard]] constexpr return_bool_t testBitNone(const std::unsigned_integral auto n) noexcept
-  {
-    static_assert(valid_variant_type<decltype(freefunc::testBitAny(n))>);
-
-    if (const auto result{ freefunc::testBitAny(n) }; result.index() != 0) {
-      return result;
-    } else {
-      return !std::get<0>(result);
-    }
-  }
-
-  [[nodiscard]] constexpr return_bool_t testBitNone(const std::unsigned_integral auto n,
-    const std::integral auto last_bits)
-  {
-    static_assert(valid_variant_type<decltype(freefunc::testBitAny(n, last_bits))>);
-
-    if (const auto result{ freefunc::testBitAny(n, last_bits) }; result.index() == 0) {
-      return !std::get<0>(result);
-    } else {
+  if constexpr (sizeof(n) == sizeof(std::uintmax_t)) {
+    if (const auto result = testBit(n, n_size); result && !*result) {
+      return false;
+    } else if (!result) {
       return result;
     }
   }
 
-  template <std::unsigned_integral U>
-  constexpr return_bool_t setBit(std::add_lvalue_reference_t<U> n, const std::integral auto pos)
-  {
-    if (!freefunc::checkBitInRange<U>(pos)) {
-        return std::domain_error("pos out of range");
-    }
-    
-    n |= (static_cast<U>(1) << pos);
-    return true;
+  const auto mask = (static_cast<std::uintmax_t>(1) << n_size) - 1;
+
+  return (n & mask) == mask;
+}
+
+// Test if an unsigned integer has last n bits set to 1
+NODISCARD constexpr
+Expected<bool> testBitAll(const unsigned_integral auto n, const integral auto last_bits)
+{
+  if (!checkBitInRange(n, last_bits)) {
+    return std::unexpected(std::domain_error("last_bits out of range."));
   }
 
-  template <std::unsigned_integral U>
-  constexpr return_bool_t setBit(std::add_lvalue_reference_t<U> n) noexcept
-  {
-    n |= static_cast<U>(-1);
-    return true;
-  }
-  
-  template <std::unsigned_integral U>
-  constexpr return_bool_t resetBit(std::add_lvalue_reference_t<U> n, const std::integral auto pos)
-  {
-    if (!freefunc::checkBitInRange<U>(pos)) {
-        return std::domain_error("pos our of range");
-    }
-    
-    static_assert(valid_variant_type<decltype(freefunc::testBit<U>(n, pos))>);
-
-    if (const auto result{ freefunc::testBit<U>(n, pos) }; result.index() != 0) {
-      return std::domain_error("freefunc::testBit function failed");
-    } else if (!std::get<0>(result)) {
-      return true;
-    }
-    
-    n ^= (static_cast<U>(1) << pos);
-    return true;
+  // If checking all the bits in front of the last bits, then delegate;
+  if (!checkBitInRange(n, last_bits + 1)) {
+    return testBitAll(n);
   }
 
-  constexpr return_bool_t resetBit(std::unsigned_integral auto& n) noexcept
-  {
-    n &= 0u;
-    return true;
+  const auto mask = (static_cast<std::uintmax_t>(1) << (last_bits + 1)) - 1;
+
+  return (n & mask) == mask;
+}
+
+// Test if any bit is set to 1
+NODISCARD constexpr
+Expected<bool> testBitAny(const unsigned_integral auto n)
+{
+  return static_cast<bool>(n);
+}
+
+// Test if any bit is set to 1
+NODISCARD constexpr
+Expected<bool> testBitAny(const unsigned_integral auto n, const integral auto last_bits)
+{
+  if (!checkBitInRange(n, last_bits)) {
+    return std::unexpected(std::domain_error("last_bits out of range"));
   }
 
-  template <std::unsigned_integral U>
-  constexpr return_bool_t flipBit(std::add_lvalue_reference_t<U> n, const std::integral auto pos)
-  {
-    if (!freefunc::checkBitInRange<U>(pos)) {
-        return std::domain_error("pos out of range");
-    }
-    
-    n ^= (static_cast<U>(1) << pos);
-    return true;
+  if (!checkBitInRange(n, last_bits + 1)) {
+    return testBitAny(n);
   }
 
-  constexpr return_bool_t flipBit(std::unsigned_integral auto& n) noexcept
-  {
-    n = ~n;
-    return true;
+  const auto mask = (static_cast<std::uintmax_t>(1) << (last_bits + 1)) - 1;
+
+  return static_cast<bool>(n & mask);
+}
+
+// Test if none of the bit is set to 1
+NODISCARD constexpr
+Expected<bool> testBitNone(const unsigned_integral auto n)
+{
+  if (const auto result = testBitAny(n); !result) {
+    return result;
+  } else {
+    return !*result;
+  }
+}
+
+// Test if none of the bit is set to 1
+NODISCARD constexpr
+Expected<bool> testBitNone(const unsigned_integral auto n, const integral auto last_bits)
+{
+  if (const auto result = testBitAny(n, last_bits); result) {
+    return !*result;
+  } else {
+    return result;
+  }
+}
+
+
+/**
+ * Bit setting functions (mutators)
+*/
+
+// Set a particular bit to 1
+template <unsigned_integral U>
+NODISCARD constexpr
+Expected<U> setBit(const U n, const integral auto pos)
+{
+  if (!checkBitInRange<U>(pos)) {
+    return std::unexpected(std::domain_error("pos out of range"));
+  } else {
+    return n | (static_cast<U>(1) << pos);
+  }
+}
+
+// Set all bit to 1
+template <unsigned_integral U>
+NODISCARD constexpr
+Expected<U> setBit(const U n) { return n | static_cast<U>(-1); }
+
+// Set a bit to 0
+template <unsigned_integral U>
+NODISCARD constexpr
+Expected<U> resetBit(const U n, const integral auto pos)
+{
+  if (!checkBitInRange<U>(pos)) {
+    return std::unexpected(std::domain_error("pos our of range"));
   }
 
-  /**
-   * The solution to solve problem with multiply invokes undefined behaviour
-   * 
-   * Explanation:
-   *  Define A & B be unsigned integers, whose size less than signed int (normally 32bits)
-   *  If the product of A * B overflows, A & B will be promoted to signed int and multiply.
-   *  However, if the result overflows, signed int will invoke undefined behaviour.
-   * 
-   * Solution:
-   *  Use template method to let unsigned integers only promote to unsigned int.
-   *  After the operation is done, truncated the result to original size.
-   * 
-  */
-
-  template <std::unsigned_integral U>
-  struct MakePromoted
-  {
-    static constexpr bool small{ sizeof(U) < sizeof(std::uint32_t) };
-
-    using type = std::conditional_t<small, std::uint32_t, U>;
-  };
-     
-  template <typename T>
-  using make_promoted_t = typename MakePromoted<T>::type;
-
-  template <typename T>
-  [[nodiscard]] inline constexpr make_promoted_t<T> promote(const T n) noexcept
-  {
+  if (const auto result = testBit<U>(n, pos); !result) {
+    return std::unexpected(std::domain_error("freefunc::testBit function failed"));
+  } else if (!*result) {
     return n;
+  } else {
+    return n ^ (static_cast<U>(1) << pos);
+  }
+}
+
+// Set all bit to 0
+NODISCARD constexpr
+auto resetBit(const unsigned_integral auto n) -> Expected<decltype(n)> { return n & 0u; }
+
+// Toggle a bit
+template <unsigned_integral U>
+NODISCARD constexpr
+Expected<U> flipBit(const U n, const integral auto pos)
+{
+  if (!checkBitInRange<U>(pos)) {
+    return std::unexpected(std::domain_error("pos out of range"));
   }
 
-  template <class T, template <class...> class Template>
-  struct is_specialization : std::false_type {};
+  return n ^ (static_cast<U>(1) << pos);
+}
 
-  template <template <class...> class Template, class... Args>
-  struct is_specialization<Template<Args...>, Template> : std::true_type {};
+// Toggle all bits
+NODISCARD constexpr
+auto flipBit(const unsigned_integral auto n) -> Expected<decltype(n)> { return ~n; }
+
+
+#undef NODISCARD
+
+/**
+ * Code ends.
+*/
+
+}
+
 }
